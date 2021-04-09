ca205a49b3483e9afc0c86aa29bd26bbaba146a1
SweetPotatoMashed <sweetpotatomashed@gmail.com>
2021-04-09 17:17:17 -0600

    Added comments and edited animations

--- TankWars/View/BeamAnimation.cs
+++ TankWars/View/BeamAnimation.cs
@@ -14,31 +14,54 @@ namespace View
 {
     public class BeamAnimation
     {
-        Vector2D origin;
-        Vector2D direction;
-        int frameNumber;
+        Vector2D origin; // The location from which the animation originates
+        Vector2D direction; // The direction the beam is traveling
+        int frameNumber; // How many frames have passed sicnce the start of the animation
 
+        /// <summary>
+        /// Creates a new beam animation
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="dir"></param>
         public BeamAnimation(Vector2D origin, Vector2D dir)
         {
             this.origin = new Vector2D(origin);
             direction = new Vector2D(dir);
+            frameNumber = 0;
         }
 
+        /// <summary>
+        /// Returns the origin of the beam animation
+        /// </summary>
+        /// <returns></returns>
         public Vector2D getOrigin()
         {
             return origin;
         }
 
+        /// <summary>
+        /// Returns the direction of the beam animation
+        /// </summary>
+        /// <returns></returns>
         public Vector2D getDirection()
         {
             return direction;
         }
 
+        /// <summary>
+        /// Returns the frame number of the beam animation
+        /// </summary>
+        /// <returns></returns>
         public int getFrameNumber()
         {
             return frameNumber;
         }
 
+        /// <summary>
+        /// Draws the beam animation
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="e"></param>
         public void BeamDrawer(object o, PaintEventArgs e)
         {
             Beam b = o as Beam;
--- TankWars/View/DeathAnimation.cs
+++ TankWars/View/DeathAnimation.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Created by Joshua Hardy and Jace Duennebeil, Spring 2021
+// Uses code written by Daniel Kopta
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,26 +12,47 @@ using System.Windows.Forms;
 
 namespace View
 {
+    /// <summary>
+    /// Class for representing tank death animations
+    /// </summary>
     public class DeathAnimation
     {
-        private Vector2D origin;
-        private int frameNumber;
+        private Vector2D origin; // The location from which the animation originates
+        private int frameNumber; // How many frames have passed sicnce the start of the animation
 
+        /// <summary>
+        /// Creates a new tank death animation
+        /// </summary>
+        /// <param name="origin"></param>
         public DeathAnimation(Vector2D origin)
         {
             this.origin = origin;
+            frameNumber = 0;
         }
 
+        /// <summary>
+        /// Returns the origin of the death animation
+        /// </summary>
+        /// <returns></returns>
         public Vector2D getOrigin()
         {
             return origin;
         }
 
+        /// <summary>
+        /// Returns the frame number of the death animation
+        /// </summary>
+        /// <returns></returns>
         public int getFrameNumber()
         {
             return frameNumber;
         }
 
+        /// <summary>
+        /// Draws the death animation
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="e"></param>
         public void DeathDrawer(object o, PaintEventArgs e)
         {
             using (SolidBrush blueBrush = new SolidBrush(Color.BlueViolet))
